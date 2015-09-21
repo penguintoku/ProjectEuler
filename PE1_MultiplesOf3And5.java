4221bbfd39f7c7d0b9a3b0af4c8f1450fe9bf080
Tokumaru Yahashi <tyahash1@binghamton.edu>
2015-09-20 23:02:36 -0400

    Multiples of 3 and 5
    https://www.hackerrank.com/contests/projecteuler/challenges/euler001

--- PE1_MultiplesOf3And5.java
+++ PE1_MultiplesOf3And5.java
@@ -17,6 +17,7 @@ public class Solution {
          * Needed to do in BigInteger since the test case on the site was large.
          */
 
+        //Initialize
         BigInteger num, sum, sub1, num3, num5, num15, t3, t5, t15;
 
         BigInteger b2 = new BigInteger("2");
@@ -27,18 +28,24 @@ public class Solution {
         for(int i = 0; i < numTestCase; ++i){
 
             num = in.nextBigInteger();
+
+            //subtract one
             sub1 = num.subtract(BigInteger.ONE);
 
+            //divide for 3, 5, and 15
             num3 = sub1.divide(b3);
             num5 = sub1.divide(b5);
             num15 = sub1.divide(b15);
 
+            //using AP Formula
             t3 = (b3.multiply(num3).multiply(num3.add(BigInteger.ONE))).divide(b2);
             t5 = (b5.multiply(num5).multiply(num5.add(BigInteger.ONE))).divide(b2);
             t15 = (b15.multiply(num15).multiply(num15.add(BigInteger.ONE))).divide(b2);
 
+            //sum is the difference
             sum = t3.add(t5).subtract(t15);
 
+            //print the solution
             System.out.println(sum);
         }
 
