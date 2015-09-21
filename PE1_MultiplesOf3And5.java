bd8fd98fd3031068585a873b3b1a3b3c4547ead4
Tokumaru Yahashi <tyahash1@binghamton.edu>
2015-09-20 22:27:53 -0400

    Solution to ProjectEuler+ on HackerRank
    For more details: https://projecteuler.net/

---
+++ PE1_MultiplesOf3And5.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int numTestCase = in.nextInt();
+
+        /**
+         *  BIGINTEGER ARE EVIL!!!!!!!
+         * Needed to do in BigInteger since the test case on the site was large.
+         */
+
+        BigInteger num, sum, sub1, num3, num5, num15, t3, t5, t15;
+
+        BigInteger b2 = new BigInteger("2");
+        BigInteger b3 = new BigInteger("3");
+        BigInteger b5 = new BigInteger("5");
+        BigInteger b15 = new BigInteger("15");
+
+        for(int i = 0; i < numTestCase; ++i){
+
+            num = in.nextBigInteger();
+            sub1 = num.subtract(BigInteger.ONE);
+
+            num3 = sub1.divide(b3);
+            num5 = sub1.divide(b5);
+            num15 = sub1.divide(b15);
+
+            t3 = (b3.multiply(num3).multiply(num3.add(BigInteger.ONE))).divide(b2);
+            t5 = (b5.multiply(num5).multiply(num5.add(BigInteger.ONE))).divide(b2);
+            t15 = (b15.multiply(num15).multiply(num15.add(BigInteger.ONE))).divide(b2);
+
+            sum = t3.add(t5).subtract(t15);
+
+            System.out.println(sum);
+        }
+
+        /**
+        int num, sum;
+
+        for(int i = 0; i < numTestCase; ++i){
+
+            num = in.nextInt();
+
+            int num3 = (num-1) / 3;
+            int num5 = (num-1) / 5;
+            int num15 = (num-1) / 15;
+
+            sum = (((3*num3*(num3+1))/2) + ((5*num5*(num5+1))/2) - ((15*num15*(num15+1))/2));
+
+            System.out.println(sum);
+        }
+        */
+    }
+}
